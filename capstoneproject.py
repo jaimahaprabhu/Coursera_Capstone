e8e2373e5b1c50764b495927d6b814473e78e302
Jaimahaprabhu A <ajmprabhu@gmail.com>
2018-11-14 20:54:57 +0530

    Add files via upload

---
+++ capstoneproject.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#this project is mainly for coursera capstone project
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
